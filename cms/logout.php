fde0864f7bb1d15cc49a581e781d494c1137edd7
Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
2012-07-04 14:50:55 +0200

    Fix clearing of session variable in logout code
    Signed-off-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>

--- cms/logout.php
+++ cms/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-unset($_SESSION['logged_in']);
+unset($_SESSION['user_id']);
 session_destroy();
 
 $site_base = substr($_SERVER['SCRIPT_NAME'], 0, strrpos($_SERVER['SCRIPT_NAME'], "/")+1);
