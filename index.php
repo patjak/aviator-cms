b16c1689a008019642aeeeba9fb30b4be58ca047
Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
2012-08-28 19:32:11 +0200

    Load theme from directory set in config.php
    Signed-off-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 $SECURE = true; // Mark this a valid entry point
 require_once("common.php");
-require_once("themes/default/config.php");
+require_once("themes/".THEME_DIR."/config.php");
 
 $theme_path = Theme::GetPath();
 $theme_base = Theme::GetBase();
 
-require_once($theme_path."page.php");
+require_once($theme_path."theme.php");
 
 ?>
