c4166c84390cf19c7d7c81efc8ef5eb736893b48
Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
2012-12-06 13:38:46 +0100

    Fix login problem with IE
    Signed-off-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>

--- cms/login.php
+++ cms/login.php
@@ -17,24 +17,24 @@ if (isset($_POST['__user']) && isset($_POST['__pass'])) {
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <link rel="stylesheet" href="style/style.css"/>
+<script type="text/javascript" src="https://getfirebug.com/firebug-lite.js"></script>
 <script type="text/javascript" src="jquery/js/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="jquery/js/jquery-ui-1.8.18.custom.min.js"></script>
 <script type="text/javascript" src="js/dialogs.js"></script>
 <script type="text/javascript" src="js/loading.js"></script>
-<!-- <link href='http://fonts.googleapis.com/css?family=Gudea:700' rel='stylesheet' type='text/css'> -->
 <title><?php echo $site_title;?> - Log in</title>
 </head>
 <body style="background-color: #dddddd;">
-<div style="text-align: center; font-size: 16pt; font-family: 'Gudea', sans-serif; margin-top: 100px;">
+<div style="text-align: center; font-size: 16pt; font-family: arial, sans-serif; margin-top: 100px;">
 
 </div>
 <div class="Box" style="width: 360px; margin-left: auto; margin-right: auto; text-align: center;">
 <h3><img src="pics/icons_32/key.png" style="margin: 10px;"/> Enter your log in information</h3>
-<form action="" method="POST" id="login_form">
+<form action="" method="POST" id="login_form" action="<?php echo SITE_BASE;?>">
 <table style="margin-left: auto; margin-right: auto; text-align: left;">
 <tr><td>Username</td><td><input type="text" name="__user" value="<?php echo $username;?>"/></td></tr>
 <tr><td>Password</td><td><input type="password" name="__pass"/></td></tr>
-<tr><td></td><td><button style="width: 150px;" onclick="LoadShow(); /*$('#login_form').submit()*/; return true;"><img src="pics/icons_24/check.png"/> Log in</button></td></tr>
+<tr><td></td><td><button style="width: 150px;" type="submit" onclick="LoadShow(); return true;"><img src="pics/icons_24/check.png"/> Log in</button></td></tr>
 </table>
 </div>
 <div id="ErrorDimmer" class="Fullscreen"></div>
