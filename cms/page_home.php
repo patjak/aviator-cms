21fc3266a3d895989c6582f174861673bf5f5c4e
Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
2014-03-14 10:59:27 +0100

    Dashboard fixes
    Signed-off-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>

--- cms/page_home.php
+++ cms/page_home.php
@@ -2,24 +2,30 @@
 <h2><img src="pics/icons_32/home.png"/> Dashboard</h2>
 <table class="Dashboard"><tr>
 <?php
-foreach (DashboardCore::GetEntries() as $entry) {
-	echo "<td>";
+$user = User::Get();
 
-	switch ($entry->GetType()) {
-	case DASHBOARD_TYPE_LINK:
-		echo "<a href=\"".$entry->GetLink()."\" target=\"_blank\">".
-		"<img src=\"".$entry->GetIcon64()."\"/><br/>".$entry->GetTitle()."</a>";
-		break;
+if ($user->limited == 1) {
+	echo "<td></td>";
+} else {
+	foreach (DashboardCore::GetEntries() as $entry) {
+		echo "<td>";
 
-	case DASHBOARD_TYPE_MODULE:
-		$plugin_id = $entry->GetPlugin()->GetId();
-		$module_id = $entry->GetModule()->GetId();
-		$type_internal_id = $entry->GetTypeInternalId();
-		echo "<a href=\"?page=".PAGE_MODULES."&plugin=".$plugin_id."&module=".$module_id."&view=".$type_internal_id."\">".
-		"<img src=\"".$entry->GetIcon64()."\"/><br/>".$entry->GetTitle()."</a>";
+		switch ($entry->GetType()) {
+		case DASHBOARD_TYPE_LINK:
+			echo "<a href=\"".$entry->GetLink()."\" target=\"_blank\">".
+			"<img src=\"".$entry->GetIcon64()."\"/><br/>".$entry->GetTitle()."</a>";
+			break;
+
+		case DASHBOARD_TYPE_MODULE:
+			$plugin_id = $entry->GetPlugin()->GetId();
+			$module_id = $entry->GetModule()->GetId();
+			$type_internal_id = $entry->GetTypeInternalId();
+			echo "<a href=\"?page=".PAGE_MODULES."&plugin=".$plugin_id."&module=".$module_id."&view=".$type_internal_id."\">".
+			"<img src=\"".$entry->GetIcon64()."\"/><br/>".$entry->GetTitle()."</a>";
+		}
+
+		echo "</td>";
 	}
-
-	echo "</td>";
 }
 ?>
 </tr>
