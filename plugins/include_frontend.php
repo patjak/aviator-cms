dd52e1fe9719c88355beb368e8b6fdedbc6d0921
Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
2014-03-14 10:57:11 +0100

    Be more clever on path parsing in the frontend include for plugins
    Still not clever enough... but works for now
    
    Signed-off-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>

--- plugins/include_frontend.php
+++ plugins/include_frontend.php
@@ -1,7 +1,19 @@
 <?php
 $SECURE = true;
+
+set_include_path(get_include_path().":../../");
+require_once("cms/common.php");
+
+// Try to figure out what relative path we're in, we solve this by searching for the '/plugins/' directory
+$plugin_dir = substr(dirname($_SERVER['PHP_SELF']), strpos($_SERVER['PHP_SELF'], "/plugins/") + strlen("/plugins/"));
+Context::SetDirectory($plugin_dir);
+
+$res = DB::Query("SELECT id FROM ".DB_PREFIX."plugins WHERE directory='".$plugin_dir."'");
+$plugin = PluginAPI::GetById($res[0]['id']);
+Context::SetPlugin($plugin);
+
 require_once("../../secure.php");
-require_once("../../cms/config.php");
+require_once("../../config.php");
 require_once("../../common.php");
 
 ?>
